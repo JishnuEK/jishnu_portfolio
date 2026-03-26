e13dfc1ed9307552404a2f239225abf57aa0f42c
Jishnu777 <jishnuekihits@gmail.com>
2026-03-26 19:48:45 +0530

    Built web
    Branch: main
    Commit: 89cfe9063e973817380daddf14b1dba2fe558acf (dirty)
    
    package:peanut 6.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a5da4958fd35ce4af7fed3e3074edf6e",
+const RESOURCES = {"flutter_bootstrap.js": "e56474d72ce976da15669d7270b5942f",
 "version.json": "c420e205d8eac4ccea99913b4a50bfa7",
 "index.html": "3ea4eb6c1793aefe62aa0364635bcf24",
 "/": "3ea4eb6c1793aefe62aa0364635bcf24",
-"main.dart.js": "80dddd47e4dd484d8ba994a7a1e57acd",
+"main.dart.js": "5ccf65555b720484c31c4eea71bc5a6c",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "a66142bfe2932272a278891cbc465ce5",
 "icons/Icon-192.png": "a66142bfe2932272a278891cbc465ce5",
